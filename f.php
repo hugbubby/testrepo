57049e03ef4af1208715e162ca3eee9c6176a2ca
ZeroPath <noreply@zeropath.com>
2024-07-14 19:41:50 +0000

    Updated the `get` key from 'asdf2' to 'asdf3' and added an echo for 'asdf4'.

--- f.php
+++ f.php
@@ -1,5 +1,6 @@
 <?php
 
-echo htmlspecialchars($_GET['asdf2'], ENT_QUOTES, 'UTF-8');
+echo htmlspecialchars($_GET['asdf3'], ENT_QUOTES, 'UTF-8');
+echo htmlspecialchars($_GET['asdf4'], ENT_QUOTES, 'UTF-8');
 
 ?>
